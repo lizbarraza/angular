05af08f2bef0ea0430a8994c862194cdeceeed76
Liz Barraza <liz.barraza@gmail.com>
2018-09-25 15:58:53 -0700

    first configuration Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+
+//Component decorator
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root',					//app-root tag in index.html This view of this component should be display here 
+  templateUrl: './app.component.html',	//template that defines the view for this component
+  styleUrls: ['./app.component.scss']	//all the css code for the root component
 })
 export class AppComponent {
   title = 'firstApp';
